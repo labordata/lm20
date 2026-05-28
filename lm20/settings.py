47d9819549cdbb1444d2543adee59c5c335545dd
Hannah Cushman Garland <hannah.cushman@datamade.us>
2026-05-28 13:52:38 -0500

    Add some debugging for build failure

--- lm20/settings.py
+++ lm20/settings.py
@@ -22,7 +22,7 @@ SPIDER_CONTRACTS = {
 # USER_AGENT = 'lm20 (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
