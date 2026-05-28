b2c17d4157c395dbdc2eb17a740b7dcfad5df014
Hannah Cushman Garland <hannah.cushman@datamade.us>
2026-05-28 14:39:15 -0500

    NVM

--- lm20/settings.py
+++ lm20/settings.py
@@ -33,9 +33,9 @@ FILE_EXPIRES = 0
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 4
+# CONCURRENT_REQUESTS_PER_DOMAIN = 4
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
